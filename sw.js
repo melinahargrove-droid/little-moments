e0e3b7779d065c09d0f0da5fa6bf1f84c25a9db4
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 18:57:21 -0500

    Refresh Little Moments service worker cache for v0.29

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='little-moments-v21';
-const ASSETS=['./','./index.html','./styles.css','./home-fit.css','./app.js','./version-badge.js','./db.js','./manifest.webmanifest','./lm%20background.png','./lm%20camera.png','./assets/home-moments.svg','./assets/home-portfolio.svg','./assets/home-botanical.svg','./assets/home-empty-moments.svg'];
+const CACHE='little-moments-v29';
+const ASSETS=['./','./index.html','./styles.css','./home-fit.css','./moments-controls.css','./app.js','./version-badge.js','./moments-controls.js','./entry-tap-fix.js','./db.js','./manifest.webmanifest','./lm%20background.png','./lm%20camera.png','./lm%20moments.png','./lm%20scrapbook.png','./assets/home-moments.svg','./assets/home-portfolio.svg','./assets/home-botanical.svg','./assets/home-empty-moments.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
 self.addEventListener('fetch',event=>{
