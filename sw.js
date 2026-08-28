eb7f11fa517a389c0accdc96c9c531249f2539c8
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 16:13:08 -0500

    Refresh cache for Home layout fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='little-moments-v4';
+const CACHE='little-moments-v5';
 const ASSETS=['./','./index.html','./styles.css','./home-fit.css','./app.js','./db.js','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
