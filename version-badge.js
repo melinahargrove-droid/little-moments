1ea79605d0fd7b44eafb32f4da2b568bf609d4b6
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 17:50:22 -0500

    Bump opening screen version to v0.21

--- version-badge.js
+++ version-badge.js
@@ -1,4 +1,4 @@
-const LM_VERSION='0.20';
+const LM_VERSION='0.21';
 function ensureVersionBadge(){
   const cover=document.querySelector('.cover');
   let badge=document.querySelector('.lm-version-badge');
