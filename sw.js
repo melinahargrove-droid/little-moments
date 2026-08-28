ef663e345f97b6f749563ecabb59dbb1843e814c
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 15:44:52 -0500

    Refresh offline cache for Class & Students build

--- sw.js
+++ sw.js
@@ -1,8 +1,27 @@
-const CACHE='little-moments-v1';
+const CACHE='little-moments-v2';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./db.js','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html')))
+  );
 });
