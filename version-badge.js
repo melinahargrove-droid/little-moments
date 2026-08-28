279e61dfbf90d9d23a5af27f609fac3e7b686552
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 17:58:37 -0500

    Set Little Moments version 0.22

--- version-badge.js
+++ version-badge.js
@@ -1,4 +1,4 @@
-const LM_VERSION='0.21';
+const LM_VERSION='0.22';
 function ensureVersionBadge(){
   const cover=document.querySelector('.cover');
   let badge=document.querySelector('.lm-version-badge');
