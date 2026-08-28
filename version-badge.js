43894b6d601c9ec6e7f4e524324b7230662dfded
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 18:59:15 -0500

    Bump Little Moments version badge to 0.30

--- version-badge.js
+++ version-badge.js
@@ -1,4 +1,4 @@
-const LM_VERSION='0.29';
+const LM_VERSION='0.30';
 function ensureVersionBadge(){
   const cover=document.querySelector('.cover');
   let badge=document.querySelector('.lm-version-badge');
