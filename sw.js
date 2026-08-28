723f11d5337684e82ca18a3a59aed9c209b3ffec
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 15:41:04 -0500

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='little-moments-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./db.js','./manifest.webmanifest'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
