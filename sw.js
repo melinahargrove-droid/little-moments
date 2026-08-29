c76da874ea71165438cc19a0f6f33317fc648bfd
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 19:07:14 -0500

    Refresh Little Moments cache for v33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='little-moments-v32';
+const CACHE='little-moments-v33';
 const ASSETS=['./','./index.html','./styles.css','./home-fit.css','./moments-controls.css','./scroll-compat.js','./sizing-entry.js','./app.js','./version-badge.js','./moments-controls.js','./db.js','./manifest.webmanifest','./lm%20background.png','./lm%20camera.png','./lm%20moments.png','./lm%20scrapbook.png','./assets/home-moments.svg','./assets/home-portfolio.svg','./assets/home-botanical.svg','./assets/home-empty-moments.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()])));
