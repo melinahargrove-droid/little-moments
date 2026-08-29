723e2555ca11b90fe071ad0f1aaf1171c8e16ef9
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-28 19:12:40 -0500

    Restore clean version badge and bump to 0.35

--- version-badge.js
+++ version-badge.js
@@ -1,4 +1,4 @@
-const LM_VERSION='0.34';
+const LM_VERSION='0.35';
 function ensureVersionBadge(){
   const cover=document.querySelector('.cover');
   let badge=document.querySelector('.lm-version-badge');
